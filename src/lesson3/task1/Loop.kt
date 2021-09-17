82a9a0af929fe40510dff9999ec0de669c86212d
user <yarivastudy@yandex.ru>
2021-09-17 18:56:42 +0300

    Task 3: Bug fixes (2)

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -2,10 +2,7 @@
 
 package lesson3.task1
 
-import kotlin.math.PI
-import kotlin.math.abs
-import kotlin.math.pow
-import kotlin.math.sqrt
+import kotlin.math.*
 
 // Урок 3: циклы
 // Максимальное количество баллов = 9
@@ -281,7 +278,7 @@ fun nonUsualCos(x: Double, eps: Double): Double {
         }
         i++
     } while (memberOfNumber > eps)
-    return rez
+    return min(rez, 1.0)
 }
 
 fun cos(x: Double, eps: Double): Double {
